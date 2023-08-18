95e53227f5089908be0318185f6ac0093c586e22
김용민 <dydals3440@gmail.com>
2023-08-18 20:24:40 +0900

    fix: 배포 수정

--- Main.js
+++ Main.js
@@ -3,14 +3,14 @@ const logo = document.querySelector('.logo');
 const loginContainer = document.querySelector('.loginContainer');
 
 logo.addEventListener('click', () => {
-  window.location.assign('http://127.0.0.1:5500/Main.html');
+  window.location.assign('http://127.0.0.1:5500/index.html');
 });
 
 // Login
 
 const login = document.querySelector('.login');
 login.addEventListener('click', () => {
-  window.location.assign('http://127.0.0.1:5500/Login/login.html');
+  window.location.assign('http://127.0.0.1:5500/Login/index.html');
 });
 
 // 로그인 성공시, 로그인 버튼을 => 로그인 유저 닉네임으로 변경
@@ -18,7 +18,7 @@ const token = localStorage.getItem('accessToken');
 const nickname = localStorage.getItem('nickname');
 
 if (token) {
-  loginContainer.innerHTML = `<a href='http://127.0.0.1:5500/MyPage/MyPage.html'> ${nickname} 님</a>`;
+  loginContainer.innerHTML = `<a href='http://127.0.0.1:5500/MyPage/index.html'> ${nickname} 님</a>`;
 } else {
-  loginContainer.innerHTML = `<a href='http://127.0.0.1:5500/Login/login.html' class='login'>로그인</a>`;
+  loginContainer.innerHTML = `<a href='http://127.0.0.1:5500/Login/index.html' class='login'>로그인</a>`;
 }
