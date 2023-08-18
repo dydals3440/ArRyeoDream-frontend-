54998d5c2e91c4320a0a182c9281271b1237a540
김용민 <dydals3440@gmail.com>
2023-08-18 20:30:15 +0900

    fix: netlify 경로 수정

--- Main.js
+++ Main.js
@@ -3,14 +3,16 @@ const logo = document.querySelector('.logo');
 const loginContainer = document.querySelector('.loginContainer');
 
 logo.addEventListener('click', () => {
-  window.location.assign('http://127.0.0.1:5500/index.html');
+  window.location.assign('https://aesthetic-rabanadas-d6196b.netlify.app/');
 });
 
 // Login
 
 const login = document.querySelector('.login');
 login.addEventListener('click', () => {
-  window.location.assign('http://127.0.0.1:5500/Login/index.html');
+  window.location.assign(
+    'https://aesthetic-rabanadas-d6196b.netlify.app/login'
+  );
 });
 
 // 로그인 성공시, 로그인 버튼을 => 로그인 유저 닉네임으로 변경
@@ -18,7 +20,8 @@ const token = localStorage.getItem('accessToken');
 const nickname = localStorage.getItem('nickname');
 
 if (token) {
-  loginContainer.innerHTML = `<a href='http://127.0.0.1:5500/MyPage/index.html'> ${nickname} 님</a>`;
+  loginContainer.innerHTML = `<a href='https://aesthetic-rabanadas-d6196b.netlify.app/'> ${nickname} 님</a>`;
 } else {
-  loginContainer.innerHTML = `<a href='http://127.0.0.1:5500/Login/index.html' class='login'>로그인</a>`;
+  loginContainer.innerHTML = `<a href='https://aesthetic-rabanadas-d6196b.netlify.app/login'
+  );' class='login'>로그인</a>`;
 }
